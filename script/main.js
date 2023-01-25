dab9ac28fa6e251a4dd462aa0ceddf83a32192ec
Geralt-of-Livia <46412117+Geralt-of-Livia@users.noreply.github.com>
2023-01-25 11:54:59 +0800

    Update main.js

--- script/main.js
+++ script/main.js
@@ -298,7 +298,7 @@ const animationTimeline = () => {
   // Restart Animation on click
   const replyBtn = document.getElementById("replay");
   replyBtn.addEventListener("click", () => {
-    tl.restart();
+    window.location.replace("https://geralt-of-livia.github.io/birthday-card/")
   });
 };
 
